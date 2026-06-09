2f98d15965a26a46019dbe3e101862677cbaac8d
jesustissera <jesustissera@gmail.com>
2026-06-08 22:39:19 -0300

    tarea (tipos): crear archivo para importar tipo noticias

---
+++ src/types/news.ts
@@ -0,0 +1,11 @@
+export interface News {
+  id?: number;
+  category?: string;
+  title?: string;
+  text?: string;
+  date?: string;
+  documentId?: string;
+  image?: {
+    url?: string;
+  };
+}
