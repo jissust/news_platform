9bfa0f2ba717f8c715bd63a08ea6b9160552b411
jesustissera <jesustissera@gmail.com>
2026-06-09 23:23:55 -0300

    Tarea (videos): Agregar bloque para mostrar ultimos videos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,19 @@ import { Hero } from "@/components/features/home/Hero/Hero";
 import { LatestNews } from "@/components/features/home/LatestNews/LatestNews";
 import { getLatestNews } from "@/services/news";
 import { getFeaturedNews } from "@/services/featured_news";
+import { LatestVideos } from "@/components/features/home/LatestVideos/LatestVideos";
+import { getLatestVideos } from "@/services/youtube";
 
 export default async function Home() {
   const news = await getLatestNews();
   const featuredNews = await getFeaturedNews();
+  const videos = await getLatestVideos(`${process.env.YOUTUBE_CHANNEL_ID}`);
 
   return (
     <>
       <Hero slides={featuredNews} />
       <LatestNews news={news.slice(0, 3)} />
+      <LatestVideos videos={videos}   />
     </>
-  
   );
 }
