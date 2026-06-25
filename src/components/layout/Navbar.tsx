71ce5830a932f4d6b1c38d14574626801456e211
jesustissera <jesustissera@gmail.com>
2026-06-25 16:23:00 -0300

    Refactor (Navbar): Arreglo en dirección de logo

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,35 +1,17 @@
 "use client";
 
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { NavLinks } from "@/components/ui/Navigation/NavLink";
-import { usePathname } from "next/navigation";
 import { SocialNetworks } from "@/components/ui/SocialNetworks/SocialNetworks";
 
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [scrolled, setScrolled] = useState(false);
-  const pathname = usePathname();
-  const lang = pathname.split("/")[1] || "es";
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrolled(window.scrollY > 50);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
-  const handleContactClick = (method: string) => {
-    console.log(method);
-    //sendGTMEvent({ event: "contact_click", method, location: "footer" });
-  };
 
   return (
     <nav
-      className={`w-full text-white px-6 py-2 fixed top-0 z-50 transition-colors duration-300 ${scrolled || isOpen ? "bg-black" : "bg-transparent"}`}
+      className={`w-full text-white px-6 py-2 top-0 z-50 transition-colors duration-300 bg-black fixed`}
     >
       <div className="max-w-7xl mx-auto grid grid-cols-2 md:grid-cols-3">
         <h1
@@ -37,7 +19,7 @@ export const Navbar = () => {
           data-aos="fade-right"
           data-aos-delay="100"
         >
-          <Link href={`/${lang}`} className="hover:text-gray-300">
+          <Link href={`/`} className="hover:text-gray-300">
             LOGO
           </Link>
         </h1>
