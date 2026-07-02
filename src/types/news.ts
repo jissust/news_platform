53e1cb56f4c78777aaa769858da821a1fb77d95f
jesustissera <jesustissera@gmail.com>
2026-07-02 14:21:40 -0300

    Refactor: agrego categorias a las noticias

--- src/types/news.ts
+++ src/types/news.ts
@@ -1,12 +1,17 @@
-export interface News {
-  id?: number;
-  category?: string;
-  title?: string;
-  text?: string;
-  date?: string;
-  documentId?: string;
-  slug: string;
-  image?: {
-    url?: string;
-  };
-}
+  export interface News {
+    id?: number;
+    category?: string;
+    title?: string;
+    text?: string;
+    date?: string;
+    documentId?: string;
+    slug: string;
+    image?: {
+      url?: string;
+    };
+    categories?: {
+      id: number;
+      title: string;
+      slug?: string;
+    }[];
+  }
